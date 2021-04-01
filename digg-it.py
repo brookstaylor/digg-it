8daf8b8a8f43b179a0bfc957edfeb30e0bfe28a4
brookstaylor <brooks.o.taylor@vanderbilt.edu>
2021-03-31 21:04:46 -0400

    remove api key

--- digg-it.py
+++ digg-it.py
@@ -14,7 +14,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger('digg-it')
 logger.setLevel(logging.DEBUG)
 
-ETHERSCAN_API_KEY = "***REMOVED***"
+ETHERSCAN_API_KEY = ""
 DIGG_ADDRESS = "0x798d1be841a82a273720ce31c822c61a67a601c3"
 USDC_ADDRESS = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
 WBTC_ADDRESS = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
